bbcb9ecf14ad44ce3998df41633d962018fe733f
jackgilbertx <jackgilbertx@gmail.com>
2020-07-08 15:49:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const burger = document.getElementById("burger");
-const dropdown = document.getElementById("dropdown");
-const tags = document.getElementById("dropdown").children;
+const burger = document.getElementById('burger');
+const dropdown = document.getElementById('dropdown');
+const tags = document.getElementsByTagName('li');
 
 // dropdown.style.height = "0px";
 burger.onclick = function () {
-  if (dropdown.style.height == "0px") {
-    dropdown.style.height = "180px";
+  if (dropdown.style.height == '0px') {
+    dropdown.style.height = '180px';
   } else {
-    dropdown.style.height = "0px";
+    dropdown.style.height = '0px';
   }
 };
 for (let i = 0; i < tags.length; i++) {
-    tags[i].onclick = function() {
-      dropdown.style.height = "0px";
-    };
+  tags[i].onclick = function () {
+    dropdown.style.height = '0px';
+  };
 }
