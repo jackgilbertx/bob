6f86aa6070bad3ffd505e2fb78496024f380f892
jackgilbertx <jackgilbertx@gmail.com>
2020-07-08 15:41:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const burger = document.getElementById("burger");
 const dropdown = document.getElementById("dropdown");
 const tags = document.getElementById("dropdown").children;
 
-dropdown.style.height = "0px";
-console.log(burger);
+// dropdown.style.height = "0px";
 burger.onclick = function () {
   if (dropdown.style.height == "0px") {
-    dropdown.style.height = "200px";
+    dropdown.style.height = "180px";
   } else {
     dropdown.style.height = "0px";
   }
 };
-console.log(tags);
-
 for (let i = 0; i < tags.length; i++) {
     tags[i].onclick = function() {
       dropdown.style.height = "0px";
